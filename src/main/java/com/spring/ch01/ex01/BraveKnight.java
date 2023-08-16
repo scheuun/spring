34dc8be61f94012ff99111e17436d71ece233cab
성채은 <schn1115@kakao.com>
2023-08-16 23:52:30 +0900

    애스팩트 적용

--- src/main/java/com/spring/ch01/ex01/BraveKnight.java
+++ src/main/java/com/spring/ch01/ex01/BraveKnight.java
@@ -1,13 +1,18 @@
 package com.spring.ch01.ex01;
 
 public class BraveKnight implements Knight {
-    private Quest quest;
 
-    public BraveKnight(Quest quest) {
+    private Quest quest;
+    private Minstrel minstrel;
+
+    public BraveKnight(Quest quest, Minstrel minstrel) {
         this.quest = quest;
+        this.minstrel = minstrel;
     }
 
     public void embarkOnQuest() {
+        minstrel.singBeforeQuest();
         quest.embark();
+        minstrel.singAfterQuest();
     }
 }
--- src/main/java/com/spring/ch01/ex01/KnightMain.java
+++ src/main/java/com/spring/ch01/ex01/KnightMain.java
@@ -6,10 +6,9 @@ public class KnightMain {
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext(
-                        "META-INF/spring/knight.xml");
+                        "META-INF/spring/minstrel.xml");
         Knight knight = context.getBean(Knight.class);
         knight.embarkOnQuest();
         context.close();
-        System.out.println("ㅆㅂ");
     }
 }
