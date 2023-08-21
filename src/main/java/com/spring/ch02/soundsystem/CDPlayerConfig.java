c1fc03782a9ea092eedb4d0eb9965996371ede8d
성채은 <schn1115@kakao.com>
2023-08-21 23:29:11 +0900

    자바로 빈 와이어링하기

--- src/main/java/com/spring/ch02/soundsystem/CDPlayerConfig.java
+++ src/main/java/com/spring/ch02/soundsystem/CDPlayerConfig.java
@@ -1,5 +1,7 @@
 package com.spring.ch02.soundsystem;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,4 +10,37 @@ import org.springframework.context.annotation.Configuration;
 //@ComponentScan(basePackages = "com.spring.ch02.soundsystem")
 //@ComponentScan(basePackages = CDPlayer.class, DVDPlayer.class)
 public class CDPlayerConfig {
+
+    @Bean(name = "lonelyHeartsClubBand")
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public CompactDisc randomBeatlesCD() {
+        int choice = (int) Math.floor(Math.random() * 4);
+        if (choice == 0) {
+            return new SgtPeppers();
+        } else if (choice == 1) {
+            return new WhiteAlbum();
+        } else {
+            return new Revolver();
+        }
+    }
+
+//    @Bean
+//    public CDPlayer cdPlayer() {
+//        return new CDPlayer(sgtPeppers());
+//    }
+
+
+    @Bean
+    public CDPlayer cdPlayer(CompactDisc compactDisc) {
+        return new CDPlayer(compactDisc);
+    }
+
+    @Bean
+    public CDPlayer anotherCDPlayer() {
+        return new CDPlayer(sgtPeppers());
+    }
 }
