7743cc5740bc55c418d98a69d874de48621e2429
성채은 <schn1115@kakao.com>
2023-09-01 23:26:43 +0900

    빈 프로파일 설정하기

--- src/main/java/com/spring/ch03/myapp/DevelopmentProfileConfig.java
+++ src/main/java/com/spring/ch03/myapp/DevelopmentProfileConfig.java
@@ -1,18 +1,19 @@
 package com.spring.ch03.myapp;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.jndi.JndiObjectFactoryBean;
 
 import javax.sql.DataSource;
 
-public class DataSourceConfig {
+@Configuration
+@Profile("dev")
+public class DevelopmentProfileConfig {
 
     @Bean(destroyMethod = "shutdown")
-    @Profile("dev")
-    public DataSource embeddedDataSource() {
+    public DataSource dataSource() {
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript("classpath:schema.sql")
@@ -20,14 +21,4 @@ public class DataSourceConfig {
                 .build();
     }
 
-    @Bean
-    @Profile("prod")
-    public DataSource jndiDataSource() {
-        JndiObjectFactoryBean jndiObjectFactoryBean =
-            new JndiObjectFactoryBean();
-        jndiObjectFactoryBean.setJndiName("jdbc/myDS");
-        jndiObjectFactoryBean.setResourceRef(true);
-        jndiObjectFactoryBean.setProxyInterface(javax.sql.DataSource.class);
-        return (DataSource) jndiObjectFactoryBean.getObject();
-    }
 }
