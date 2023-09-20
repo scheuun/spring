67c605b45fbbde3b425428c792d969d890e2256c
성채은 <schn1115@kakao.com>
2023-09-20 16:14:34 +0900

    애스팩트 정의하기

---
+++ src/main/java/com/spring/ch04/concert/Audience.java
@@ -0,0 +1,32 @@
+package com.spring.ch04.concert;
+
+import org.aspectj.lang.annotation.*;
+import org.springframework.context.annotation.Bean;
+
+@Aspect
+public class Audience {
+
+    @Pointcut("execution(**concert.Performance.perform(..))")
+    public void performance() {}
+
+    @Before("performance()")
+    public void silenceCellPhones() {
+        System.out.println("Silencing cell phones");
+    }
+
+    @Before("performance()")
+    public void takeSeats() {
+        System.out.println("Taking Seats");
+    }
+
+    @AfterReturning("performance()")
+    public void applause() {
+        System.out.println("CLAP CLAP CLAP!!!");
+    }
+
+    @AfterThrowing("performance()")
+    public void demandRefund() {
+        System.out.println("Demanding a refund");
+    }
+
+}
---
+++ src/main/java/com/spring/ch04/concert/Performance.java
@@ -0,0 +1,5 @@
+package com.spring.ch04.concert;
+
+public interface Performance {
+    public void perform();
+}
