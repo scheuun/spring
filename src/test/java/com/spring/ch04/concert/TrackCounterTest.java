149aa158b1f34c4230803b99a2c970903535fa6f
성채은 <schn1115@kakao.com>
2023-09-21 16:50:17 +0900

    어드바이스에서 파라미터 처리하기

---
+++ src/test/java/com/spring/ch04/concert/TrackCounterTest.java
@@ -0,0 +1,48 @@
+package com.spring.ch04.concert;
+
+import com.spring.ch02.xmlconfig.CompactDisc;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TrackCounterConfig.class)
+public class TrackCounterTest {
+
+    @Rule
+    public final StandardOutputStreamLog log =
+            new StandardOutputStreamLog();
+
+    @Autowired
+    private CompactDisc cd;
+
+    @Autowired
+    private TrackCounter counter;
+
+    @Test
+    public void testTrackCounter() {
+        cd.playTrack(1);
+        cd.playTrack(2);
+        cd.playTrack(3);
+        cd.playTrack(3);
+        cd.playTrack(3);
+        cd.playTrack(3);
+        cd.playTrack(7);
+        cd.playTrack(7);
+
+        assertEquals(1, counter.getPlayCount(1));
+        assertEquals(1, counter.getPlayCount(2));
+        assertEquals(1, counter.getPlayCount(3));
+        assertEquals(1, counter.getPlayCount(4));
+        assertEquals(1, counter.getPlayCount(5));
+        assertEquals(1, counter.getPlayCount(6));
+        assertEquals(1, counter.getPlayCount(7));
+    }
+
+}
