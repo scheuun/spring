81f78801d75c7c59e4917e96855fbeabd08f4a91
성채은 <schn1115@kakao.com>
2023-09-25 15:03:21 +0900

    before 어드바이스와 after 어드바이스 선언하기

--- src/main/java/com/spring/ch04/concert/Audience.java
+++ src/main/java/com/spring/ch04/concert/Audience.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.Bean;
 @Aspect
 public class Audience {
 
-    @Pointcut("execution(**concert.Performance.perform(..))")
-    public void performance() {}
+//    @Pointcut("execution(**concert.Performance.perform(..))")
+//    public void performance() {}
 
 //    @Before("performance()")
 //    public void silenceCellPhones() {
@@ -30,15 +30,31 @@ public class Audience {
 //        System.out.println("Demanding a refund");
 //    }
 
-    @Around("performance()")
-    public void watchPerformance(ProceedingJoinPoint jp) {
-        try {
-            System.out.println("Silencing cell phones"); System.out.println("Taking Seats");
-            jp.proceed();
-            System.out.println("CLAP CLAP CLAP!!!");
-        } catch (Throwable e) {
-            System.out.println("Demanding a refund");
-        }
+//    @Around("performance()")
+//    public void watchPerformance(ProceedingJoinPoint jp) {
+//        try {
+//            System.out.println("Silencing cell phones"); System.out.println("Taking Seats");
+//            jp.proceed();
+//            System.out.println("CLAP CLAP CLAP!!!");
+//        } catch (Throwable e) {
+//            System.out.println("Demanding a refund");
+//        }
+//    }
+
+    public void silenceCellPhones() {
+        System.out.println("Silencing cell phones");
+    }
+
+    public void takeSeats() {
+        System.out.println("Taking Seats");
+    }
+
+    public void applause() {
+        System.out.println("CLAP CLAP CLAP!!!");
+    }
+
+    public void demandRefund() {
+        System.out.println("Demanding a refund");
     }
 
 }
