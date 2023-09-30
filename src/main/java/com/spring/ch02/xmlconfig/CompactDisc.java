0671ef77fdaaff49b1d797cac87b0e1421b37e51
성채은 <schn1115@kakao.com>
2023-09-30 23:44:05 +0900

    간단한 컨트롤러 작성하기

--- src/main/java/com/spring/ch02/xmlconfig/CompactDisc.java
+++ src/main/java/com/spring/ch02/xmlconfig/CompactDisc.java
@@ -2,4 +2,6 @@ package com.spring.ch02.xmlconfig;
 
 public interface CompactDisc {
     void play();
+
+    void playTrack(int i);
 }
--- src/main/java/com/spring/ch05/config/WebConfig.java
+++ src/main/java/com/spring/ch05/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.spring.ch05.config;
 
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
